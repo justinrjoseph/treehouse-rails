95c16827218113e66951a66659ff7414bbf598c8
Justin Joseph <justin.joseph0@yahoo.com>
2015-08-15 21:03:38 -0400

    Add specs for deleting to-dos

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Creating todo lists" do
+
   def create_todo_list(options = {})
     options[:title] ||= "My todo list"
     options[:description] ||= "This is my todo list."
@@ -20,48 +21,48 @@ describe "Creating todo lists" do
   end
 
   it "displays an error when the todo list has no title" do
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       create_todo_list title: ""
 
       expect(page).to have_content "error"
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       visit "/todo_lists"
       expect(page).to_not have_content "This is what I'm doing today."
   end
 
   it "displays an error when the todo list has title less than 3 characters" do
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       create_todo_list title: "Hi"
 
       expect(page).to have_content "error"
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       visit "/todo_lists"
       expect(page).to_not have_content "This is what I'm doing today."
   end
 
   it "displays an error when the todo list has no description" do
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       create_todo_list title: "Grocery list", description: ""
 
       expect(page).to have_content "error"
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       visit "/todo_lists"
       expect(page).to_not have_content "Grocery list"
   end
 
   it "displays an error when the todo list has description less than 5 characters" do
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       create_todo_list title: "Grocery list", description: "Food"
 
       expect(page).to have_content "error"
-      expect(TodoList.count).to eq(0)
+      expect(TodoList.count).to eq 0
 
       visit "/todo_lists"
       expect(page).to_not have_content "Grocery list"
